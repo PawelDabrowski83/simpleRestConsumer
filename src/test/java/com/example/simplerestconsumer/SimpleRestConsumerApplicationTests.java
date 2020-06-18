cab7529967a69721d2a6f11cd7da363fe8363215
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-18 14:12:38 +0200

    through template

---
+++ src/test/java/com/example/simplerestconsumer/SimpleRestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
